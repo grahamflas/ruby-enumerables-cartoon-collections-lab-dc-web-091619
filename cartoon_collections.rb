42ebce078c44164dadf90858fc9859256642a67e
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-02 13:49:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+    dwarves.each_with_index {|dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(array_arg)
+    array_cap = Array.new
+    array_arg.map {|element| array_cap.push(element.capitalize + "!")}
+    return array_cap
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+    if array.count{|element| element.length > 4} > 0
+        return true
+    else
+        return false
+    end
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+# the array below is here to help
+cheese_types = ["cheddar", "gouda", "camembert"]
 end
+  
